b662a0b04e66cb63b8a52cafdbc35005282c10cc
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2023-09-08 11:44:58 -0700

    kmfk

---
+++ Operators.java
@@ -0,0 +1,30 @@
+
+/**
+ Version: 1.0.0
+ Author: D1teri or Dima K.
+ **/
+public class Operators
+{
+    int i = 10;
+    int f = 3;
+    int flag = 13;
+    String b = 
+    public void run(){
+        f = f + i ;
+        i++;
+        
+        while(flag != 0 ){
+            if(flag%2 == 0){
+            }
+            System.out.println(flag);
+        }
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+    }
+    
+    public static void main (String[] args){
+        Operators sp = new Operators();//creates a commad sp which can use memory of the system to run the code from file/class Operatos.
+        sp.run();//runs function called run()
+    }
+}
